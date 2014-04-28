d381a4c39178d1d3522ff13e98ea93e975908558
astingu <andreea.stingu@endava.com>
2014-04-28 17:04:44 +0300

    First branch commit

--- src/exemplu/Exemplu.java
+++ src/exemplu/Exemplu.java
@@ -18,7 +18,7 @@ public class Exemplu {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello");
-        
+	System.out.println("World");        
     }
     
 }
