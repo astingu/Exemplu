c7a170f35f40e50f038910d3f8275dd72e42b8b0
astingu <andreea.stingu@endava.com>
2014-04-28 16:59:00 +0300

    Second commit

--- src/exemplu/Exemplu.java
+++ src/exemplu/Exemplu.java
@@ -17,6 +17,8 @@ public class Exemplu {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello");
+        
     }
     
 }
